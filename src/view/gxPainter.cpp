d8a7038f2e33d394ed51126c7dba2ac75a2cbde8
Roey Izhaki <roey@gefri.org>
2013-02-13 01:52:26 +0000

    Code Styling of gxPainter

--- src/view/gxPainter.cpp
+++ src/view/gxPainter.cpp
@@ -3,154 +3,160 @@
 #include "core/gxLog.h"
 
 gxPainter::gxPainter()
-: mNeedsTranslating(false),
-  mNeedsScaling(false),
-  mNeedsScrolling(false),
-  mTransformEnabledFlags(gxTransformFlags::All)
+  : mNeedsTranslating( false ),
+    mNeedsScaling( false ),
+    mNeedsScrolling( false ),
+    mTransformEnabledFlags( gxTransformFlags::All )
 {
 }
 
 void gxPainter::UpdateTransformationsNeeded()
 {
-  mNeedsTranslating = ( (mTrans.Translate.X != 0) || (mTrans.Translate.Y != 0) ) &&
-                      mTransformEnabledFlags.IsSet(gxTransformFlags::Translate);
+    mNeedsTranslating =
+        ( ( mTrans.Translate.X != 0 ) || ( mTrans.Translate.Y != 0 ) ) &&
+         mTransformEnabledFlags.IsSet( gxTransformFlags::Translate );
 
-  mNeedsScrolling = ( (mTrans.Scroll.X != 0) || (mTrans.Scroll.Y != 0) ) &&
-                    mTransformEnabledFlags.IsSet(gxTransformFlags::Scroll);
+    mNeedsScrolling =
+        ( ( mTrans.Scroll.X != 0 ) || ( mTrans.Scroll.Y != 0 ) ) &&
+        mTransformEnabledFlags.IsSet( gxTransformFlags::Scroll );
 
-  mNeedsScaling = ( (mTrans.Scale.X != 1) || (mTrans.Scale.Y != 1) ) &&
-                  mTransformEnabledFlags.IsSet(gxTransformFlags::Scale);
+    mNeedsScaling =
+        ( ( mTrans.Scale.X != 1 ) || ( mTrans.Scale.Y != 1 ) ) &&
+        mTransformEnabledFlags.IsSet( gxTransformFlags::Scale );
 }
 
-void gxPainter::SetTranslate(gxPix dx, gxPix dy)
+void gxPainter::SetTranslate( gxPix dx,
+                              gxPix dy )
 {
-  if (mNeedsScaling)
-  {
-    // Take into account any scaling that is in force.
-    // Say the value given is (40,40), with a scale set to 2 the resultant
-    // position will be (80,80). Makes sense innit?
-    mTrans.Translate.X += gxFloor( dx * mTrans.Scale.X);
-    mTrans.Translate.Y += gxFloor( dy * mTrans.Scale.Y);
-  } else {
-    mTrans.Translate.X += dx;
-    mTrans.Translate.Y += dy;
-  }
+    if ( mNeedsScaling )
+    {
+        // Take into account any scaling that is in force.
+        // Say the value given is (40,40), with a scale set to 2 the resultant
+        // position will be (80,80). Makes sense innit?
+        mTrans.Translate.X += gxFloor( dx * mTrans.Scale.X );
+        mTrans.Translate.Y += gxFloor( dy * mTrans.Scale.Y );
+    } else {
+        mTrans.Translate.X += dx;
+        mTrans.Translate.Y += dy;
+    }
 
-  UpdateTransformationsNeeded();
+    UpdateTransformationsNeeded();
 }
 
-void gxPainter::SetScroll(gxPix sx, gxPix sy)
+void gxPainter::SetScroll( gxPix sx,
+                           gxPix sy )
 {
-  if (mNeedsScaling)
-  {
-    // Take into account any scaling that is in force.
-    // Say the value given is (40,40), with a scale set to 2 the resultant
-    // position will be (80,80). Makes sense innit?
-    mTrans.Scroll.X += gxFloor( sx * mTrans.Scale.X);
-    mTrans.Scroll.Y += gxFloor( sy * mTrans.Scale.Y);
-  } else {
-    mTrans.Scroll.X += sx;
-    mTrans.Scroll.Y += sy;
-  }
+    if ( mNeedsScaling )
+    {
+        // Take into account any scaling that is in force.
+        // Say the value given is (40,40), with a scale set to 2 the resultant
+        // position will be (80,80). Makes sense innit?
+        mTrans.Scroll.X += gxFloor( sx * mTrans.Scale.X );
+        mTrans.Scroll.Y += gxFloor( sy * mTrans.Scale.Y );
+    } else {
+        mTrans.Scroll.X += sx;
+        mTrans.Scroll.Y += sy;
+    }
 
-  UpdateTransformationsNeeded();
+    UpdateTransformationsNeeded();
 }
 
-void gxPainter::SetScale(gxScale const &aScale)
+void gxPainter::SetScale( gxScale const &aScale )
 {
-  mTrans.Scale.X *= aScale.X;
-  mTrans.Scale.Y *= aScale.Y;
+    mTrans.Scale.X *= aScale.X;
+    mTrans.Scale.Y *= aScale.Y;
 
-  UpdateTransformationsNeeded();
+    UpdateTransformationsNeeded();
 }
 
 void gxPainter::PushState()
 {
-  gxPainterState *iState = new gxPainterState();
+    gxPainterState *iState = new gxPainterState();
 
-  iState->transformations = mTrans;
-  iState->transformEnabledFlags = mTransformEnabledFlags;
-  iState->clipArea = GetClipRect();
+    iState->transformations = mTrans;
+    iState->transformEnabledFlags = mTransformEnabledFlags;
+    iState->clipArea = GetClipRect();
 
-  mStateStack.push(iState);
+    mStateStack.push( iState );
 }
 
 void gxPainter::PopState()
 {
-  // Get the top state from the stack
-  gxPainterState *iState = mStateStack.top();
+    // Get the top state from the stack
+    gxPainterState *iState = mStateStack.top();
   
-  // Restore it
-  RestoreState(iState);
+    // Restore it
+    RestoreState( iState );
 
-  // Remove it from stack;
-  mStateStack.pop();
+    // Remove it from stack;
+    mStateStack.pop();
 
-  delete iState;
-  iState = NULL;
+    delete iState;
+    iState = NULL;
 }
 
 void gxPainter::RestoreState()
 {
-  // Get the top state from the stack
-  gxPainterState *iState = mStateStack.top();
-  // Restore it
-  RestoreState(iState);
+    // Get the top state from the stack
+    gxPainterState *iState = mStateStack.top();
+    
+    // Restore it
+    RestoreState( iState );
 }
 
-void gxPainter::RestoreState(gxPainterState *aState)
+void gxPainter::RestoreState( gxPainterState *aState )
 {
-  mTrans = aState->transformations;
-  mTransformEnabledFlags = aState->transformEnabledFlags;
-  UpdateTransformationsNeeded();
+    mTrans = aState->transformations;
+    mTransformEnabledFlags = aState->transformEnabledFlags;
+    UpdateTransformationsNeeded();
 
-  SetAbsoluteClipArea(aState->clipArea);
+    SetAbsoluteClipArea( aState->clipArea );
 }
 
-void gxPainter::SetClipArea(gxRect const &aRect)
+void gxPainter::SetClipArea( gxRect const &aRect )
 {
-  // Transforms the rect, taking into account trnaslation, scale, etc.
-  gxRect iTransformedRect(aRect);
-  Transform(iTransformedRect);
+    // Transforms the rect, taking into account trnaslation, scale, etc.
+    gxRect iTransformedRect( aRect );
+    Transform( iTransformedRect );
   
-  IntersectClipArea(iTransformedRect);
+    IntersectClipArea( iTransformedRect );
 }
 
-void gxPainter::SetTransformFlags(gxTransformFlags aFlags)
+void gxPainter::SetTransformFlags( gxTransformFlags aFlags )
 {
-  mTransformEnabledFlags = aFlags;
-  UpdateTransformationsNeeded();
+    mTransformEnabledFlags = aFlags;
+    UpdateTransformationsNeeded();
 }
 
-void gxPainter::Transform(gxRect &aRect)
+void gxPainter::Transform( gxRect &aRect )
 {
-  if (mNeedsScaling)
-    aRect.Scale(mTrans.Scale.X, mTrans.Scale.Y);
+    if ( mNeedsScaling )
+        aRect.Scale( mTrans.Scale.X, mTrans.Scale.Y );
 
-  if (mNeedsTranslating)
-    aRect.Offset(mTrans.Translate.X, mTrans.Translate.Y); 
+    if ( mNeedsTranslating )
+        aRect.Offset( mTrans.Translate.X, mTrans.Translate.Y );
 
-  if (mNeedsScrolling)
-    aRect.Offset(-mTrans.Scroll.X, -mTrans.Scroll.Y); 
+    if ( mNeedsScrolling )
+        aRect.Offset( -mTrans.Scroll.X, -mTrans.Scroll.Y );
 }
 
-void gxPainter::Transform(gxPoint &aPoint)
+void gxPainter::Transform( gxPoint &aPoint )
 {
-  if (mNeedsScaling)
-  {
-    aPoint.X = gxFloor(aPoint.X * mTrans.Scale.X);
-    aPoint.Y = gxFloor(aPoint.Y * mTrans.Scale.Y);
-  }
+    if ( mNeedsScaling )
+    {
+        aPoint.X = gxFloor( aPoint.X * mTrans.Scale.X );
+        aPoint.Y = gxFloor( aPoint.Y * mTrans.Scale.Y );
+    }
 
-  if (mNeedsTranslating)
-  {
-    aPoint.X += mTrans.Translate.X;
-    aPoint.Y += mTrans.Translate.Y;
-  }
+    if ( mNeedsTranslating )
+    {
+        aPoint.X += mTrans.Translate.X;
+        aPoint.Y += mTrans.Translate.Y;
+    }
 
-  if (mNeedsScrolling)
-  {
-    aPoint.X -= mTrans.Scroll.X;
-    aPoint.Y -= mTrans.Scroll.Y;
-  }
+    if ( mNeedsScrolling )
+    {
+        aPoint.X -= mTrans.Scroll.X;
+        aPoint.Y -= mTrans.Scroll.Y;
+    }
 }
--- src/view/gxPainter.h
+++ src/view/gxPainter.h
@@ -17,9 +17,9 @@ typedef std::stack< gxPainterState* > StateStack;
 struct gxPainterState
 {
 public:
-  gxRect            clipArea;
-  gxTransformations transformations;
-  gxTransformFlags  transformEnabledFlags;
+    gxRect            clipArea;
+    gxTransformations transformations;
+    gxTransformFlags  transformEnabledFlags;
 };
 
 /**
@@ -38,163 +38,185 @@ public:
 class gxPainter: public gxObject
 {
 public:
-  gxPainter();
-  /**
-   * @brief Stores the state of the painter in a stack.  
-   */
-  virtual void PushState();
+    gxPainter();
+    /**
+     * @brief Stores the state of the painter in a stack.
+     */
+    virtual void PushState();
 
-  /**
-   * @brief Pops the state of the painter from stack.  
-   */
-  virtual void PopState();	
+    /**
+     * @brief Pops the state of the painter from stack.
+     */
+    virtual void PopState();
 
-  /**
-   * @brief Restores the last state on the stack without popping it.  
-   */
-  virtual void RestoreState();
+    /**
+     * @brief Restores the last state on the stack without popping it.
+     */
+    virtual void RestoreState();
 
-  /**
-   * @brief Sets the translation (offset) for all drawing operations.
-   *
-   * As painters often draw view elements that are traversed recursively,
-   * painters allow moving the painting origin so view element won't have
-   * each to calculate their absolute position.
-   * @param dx The amout of pixels to shift paining origin by on the X axis.
-   * @param dy The amout of pixels to shift paining origin by on the Y axis.
-   */
-  virtual void SetTranslate(gxPix dx, gxPix dy);
+    /**
+     * @brief Sets the translation (offset) for all drawing operations.
+     *
+     * As painters often draw view elements that are traversed recursively,
+     * painters allow moving the painting origin so view element won't have
+     * each to calculate their absolute position.
+     * @param dx The amout of pixels to shift paining origin by on the X axis.
+     * @param dy The amout of pixels to shift paining origin by on the Y axis.
+     */
+    virtual void SetTranslate( gxPix dx,
+                               gxPix dy );
 
-  /**
-   * @brief Sets the scroll value for all drawing operations.
-   * @param sx The scroll value on the X axis.
-   * @param sy The scroll value on the Y axis.
-   */
-  virtual void SetScroll(gxPix sx, gxPix sy);
+    /**
+     * @brief Sets the scroll value for all drawing operations.
+     * @param sx The scroll value on the X axis.
+     * @param sy The scroll value on the Y axis.
+     */
+    virtual void SetScroll( gxPix sx,
+                            gxPix sy );
 
-  /**
-   * @brief Sets the scale (zoom) for all drawing operations.
-   * @param aScale The new scale
-   */
-  virtual void SetScale(gxScale const &aScale);
+    /**
+     * @brief Sets the scale (zoom) for all drawing operations.
+     * @param aScale The new scale
+     */
+    virtual void SetScale( gxScale const &aScale );
 
-  /**
-   * @brief Sets the clip rect for drawing while intersecting with existing
-   * clip areas.
-   *
-   * {@link gxViewElement View elements} call this so their children are
-   * clipped when painted.
-   *
-   *  No drawing will occur outside the given area.
-   * @param aRect The clip rectangle (which is relative, ie, not transformed)
-   */
-  virtual void SetClipArea(gxRect const &aRect);
+    /**
+     * @brief Sets the clip rect for drawing while intersecting with existing
+     * clip areas.
+     *
+     * {@link gxViewElement View elements} call this so their children are
+     * clipped when painted.
+     *
+     *  No drawing will occur outside the given area.
+     * @param aRect The clip rectangle (which is relative, ie, not transformed)
+     */
+    virtual void SetClipArea( gxRect const &aRect );
 
-  /**
-   * @brief Sets the absolute clip rect for drawing (without intersecting with 
-   * any existing clip areas and without any transformations). 
-   *
-   * Largly used by {@link gxPainter::RestoreState RestoreState}.
-   * No drawing will occur outside the given area.
-   * @param aRect The absolute coordinates of the clip rectangle.
-   */
-  virtual void SetAbsoluteClipArea(gxRect const &aRect) = 0;
+    /**
+     * @brief Sets the absolute clip rect for drawing (without intersecting with
+     * any existing clip areas and without any transformations).
+     *
+     * Largly used by {@link gxPainter::RestoreState RestoreState}.
+     * No drawing will occur outside the given area.
+     * @param aRect The absolute coordinates of the clip rectangle.
+     */
+    virtual void SetAbsoluteClipArea( gxRect const &aRect ) = 0;
 
-  /**
-   * @brief Returns the current (absolute) clip rectangle
-   * @return The clip rectangle
-   */
-  virtual gxRect GetClipRect() const = 0;
+    /**
+    * @brief Returns the current (absolute) clip rectangle
+    * @return The clip rectangle
+    */
+    virtual gxRect GetClipRect() const = 0;
 
-  /**
-   * @brief Checks if the passed rectangle requires repainting.
-   *
-   * Normally this is done by checking if the rect intersects with the clipped 
-   * bounds of the painting area and any damaged areas.
-   * @param aRect The rect to check (which is relative, ie, not transformed)
-   */
-  virtual bool NeedsPainting(gxRect const &aRect) = 0;
+    /**
+    * @brief Checks if the passed rectangle requires repainting.
+    *
+    * Normally this is done by checking if the rect intersects with the clipped 
+    * bounds of the painting area and any damaged areas.
+    * @param aRect The rect to check (which is relative, ie, not transformed)
+    */
+    virtual bool NeedsPainting( gxRect const &aRect ) = 0;
 
-  /**
-   * @brief Sets the transform flags for this painter, allowing clients to
-   * disable scale, scroll or translate.
-   * 
-   * As an example, view elements with absolute coordinates will disable
-   * translate.
-   * @param aFlags The new set of transform flags.
-   */
-  virtual void SetTransformFlags(gxTransformFlags aFlags); 
+    /**
+    * @brief Sets the transform flags for this painter, allowing clients to
+    * disable scale, scroll or translate.
+    * 
+    * As an example, view elements with absolute coordinates will disable
+    * translate.
+    * @param aFlags The new set of transform flags.
+    */
+    virtual void SetTransformFlags( gxTransformFlags aFlags );
 
-  // Drawing methods
-  virtual void DrawRectangle(gxPix x, gxPix y,gxPix w, gxPix h) = 0;
-  virtual void DrawRectangle(gxRect const &aRect) = 0;
+    // Drawing methods
+    virtual void DrawRectangle( gxPix x,
+                                gxPix y,
+                                gxPix w,
+                                gxPix h ) = 0;
+    
+    virtual void DrawRectangle( gxRect const &aRect ) = 0;
 
-  virtual void DrawLine(gxPix x1, gxPix y1, gxPix x2, gxPix y2, bool isHorizontal = true) = 0;
+    virtual void DrawLine( gxPix x1,
+                           gxPix y1,
+                           gxPix x2,
+                           gxPix y2,
+                           bool  isHorizontal = true ) = 0;
   
-  virtual void DrawText(gxString &aText, gxPix x, gxPix y) = 0;
-  /**
-   * @brief This method is particularly to be used by rulers.
-   * @param aText The text to be drawns.
-   * @param x The X position.
-   * @param y The Y Position.
-   * @param aPadX Padding to add to the X axis; Not subject to transformations.
-   * @param aPadY Padding to add to the Y axis; Not subject to transformations.
-   * @param isHorizontal Whether the text to be drawn horizontally or 
-   * vertically.
-   */
-  virtual void DrawText(gxString &aText, gxPix x, gxPix y, gxPix aPadX, gxPix aPadY, bool isHorizontal = true) = 0;
-  virtual void DrawRotatedText(gxString &aText, gxPix x, gxPix y, double aAngle) = 0;
-  
-  /**
-   * @brief Returns the size of the given text.
-   * @param aText The text whose size we enquire.
-   * @return The size of the given text.
-   */
-  virtual gxSize GetTextSize(gxString &aText) = 0;
+    virtual void DrawText( gxString &aText,
+                           gxPix    x,
+                           gxPix    y) = 0;
+    
+    /**
+    * @brief This method is particularly to be used by rulers.
+    * @param aText The text to be drawns.
+    * @param x The X position.
+    * @param y The Y Position.
+    * @param aPadX Padding to add to the X axis; Not subject to transformations.
+    * @param aPadY Padding to add to the Y axis; Not subject to transformations.
+    * @param isHorizontal Whether the text to be drawn horizontally or 
+    * vertically.
+    */
+    virtual void DrawText( gxString &aText,
+                           gxPix    x,
+                           gxPix    y,
+                           gxPix    aPadX,
+                           gxPix    aPadY,
+                           bool     isHorizontal = true ) = 0;
+    
+    virtual void DrawRotatedText( gxString &aText,
+                                  gxPix    x,
+                                  gxPix    y,
+                                  double   aAngle ) = 0;
+
+    /**
+    * @brief Returns the size of the given text.
+    * @param aText The text whose size we enquire.
+    * @return The size of the given text.
+    */
+    virtual gxSize GetTextSize( gxString &aText ) = 0;
 protected:
-  /**
-   * @brief Performs state restoration.  
-   */
-  virtual void RestoreState(gxPainterState *aState);
+    /**
+    * @brief Performs state restoration.  
+    */
+    virtual void RestoreState( gxPainterState *aState );
 
-  /**
-   * @brief Intersects the current clip area with a new rectangle.
-   * @param aRect The absolute coordinates of the rect to intersect.
-   */
-  virtual void IntersectClipArea(gxRect const &aRect) = 0;
+    /**
+    * @brief Intersects the current clip area with a new rectangle.
+    * @param aRect The absolute coordinates of the rect to intersect.
+    */
+    virtual void IntersectClipArea( gxRect const &aRect ) = 0;
 
-  /**
-   * @brief Transforms a {@link gxRect}, taking into account translate, scale
-   * , etc.
-   * @param aRect The rect to transform.
-   */
-  virtual void Transform(gxRect &aRect);
+    /**
+    * @brief Transforms a {@link gxRect}, taking into account translate, scale
+    * , etc.
+    * @param aRect The rect to transform.
+    */
+    virtual void Transform( gxRect &aRect );
 
-  /**
-   * @brief Transfroms a {@link gxPoint).
-   * @param aPoint The point to transform.
-   */
-  virtual void Transform(gxPoint &aPoint);
+    /**
+    * @brief Transfroms a {@link gxPoint).
+    * @param aPoint The point to transform.
+    */
+    virtual void Transform( gxPoint &aPoint );
 
-  /**
-   * @brief Works out what transformations are actually neede.
-   * 
-   * This method will update the mNeedsX members.
-   */
-  void UpdateTransformationsNeeded();
+    /**
+    * @brief Works out what transformations are actually neede.
+    * 
+    * This method will update the mNeedsX members.
+    */
+    void UpdateTransformationsNeeded();
 
-  // These are for performence optimization.
-  bool mNeedsTranslating;
-  bool mNeedsScaling;
-  bool mNeedsScrolling;
+    // These are for performence optimization.
+    bool mNeedsTranslating;
+    bool mNeedsScaling;
+    bool mNeedsScrolling;
 
-  /// The various transformations this painter applies.
-  gxTransformations mTrans;
+    /// The various transformations this painter applies.
+    gxTransformations mTrans;
 
-  /// Flags representing which transformation clients wish to be enabled.
-  gxTransformFlags mTransformEnabledFlags;
+    /// Flags representing which transformation clients wish to be enabled.
+    gxTransformFlags  mTransformEnabledFlags;
 
-  StateStack  mStateStack;
+    StateStack        mStateStack;
 };
 
 #endif // gxPainter_h
