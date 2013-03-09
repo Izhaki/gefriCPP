d053897eb0d1ad91b89863ab76006feb5dd8ec4c
Roey Izhaki <roey@gefri.org>
2013-03-09 19:58:35 +0000

    Added comments to gxConstraints

--- src/view/Layouts/Constraints/gxConstraints.h
+++ src/view/Layouts/Constraints/gxConstraints.h
@@ -8,24 +8,49 @@ class gxViewElement;
 #include "core/geometry/gxGeometry.h"
 #include "View/Layouts/Constraints/gxRatio.h"
 
+/**
+ * @brief A class representing extra details needed to layout view elements.
+ *
+ * Constraints are used by {@link gxLayout layouts}. Each constraint is
+ * linked to a view element and include extra information to help the layout
+ * position and size the element. For example, the initial rect of the element,
+ * its (possibly rational) size, its region in a border layout, and so forth.
+ */
 struct gxConstraints
 {
-    typedef std::list< gxConstraints* > List;
-    typedef typename List::iterator    Iterator;
-    
     gxConstraints( gxViewElement* aElement) : Element( aElement ) { }
+    
+    typedef std::list< gxConstraints* > List;
+    typedef typename List::iterator     Iterator;
+    
+    // The element this constraint applies to
     gxViewElement* Element;
+    
+    // The initial position and size of the element
     gxRect         Rect;
+    
+    // The size of the element in percent or flex.
     gxRatio        Ratio;
     
     
-    // A helper variable for when the layout is calculated/
+    // A helper variable for used when the layout is being calculated.
+    // Layouts only change the bounds of the view element when all layout
+    // operations have completed. Until then, the layout used this variable to
+    // store what will become the bounds of the view element once Apply() is
+    // called.
     gxRect         Bounds;
     
-    // Sets the CalcRect to the original rect
+    /**
+     * @brief Sets the bounds of the element to its initial rect.
+     */
     void Reset();
+    
+    /**
+     * @brief Applies the bounds to the view element.
+     */
     void Apply();
     
+    // Readability methods related to Percent and Flex ratios.
     bool  IsPixels()   { return Ratio.IsEmpty(); }
     bool  IsntPixels() { return Ratio.IsntEmpty(); }
     bool  IsPercent()  { return Ratio.IsntEmpty() && Ratio.IsPercent(); }
