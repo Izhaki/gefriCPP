332e7cbd48947d6107fe0e12eeb687808cbbd540
Roey Izhaki <roey@gefri.org>
2013-04-18 00:50:50 +0100

    Fixed gxAssert and optimised release version of gxLog

--- src/adapters/wx/gxAssert.h
+++ src/adapters/wx/gxAssert.h
@@ -4,9 +4,10 @@
 // Notice that gxAssert uses an opposite condition to wx.
 // gxAssert will generate assertion if the condition is true.
 #if defined(__cplusplus) && defined(__WXDEBUG__)
-  #define gxAssert(cond, msg) wxASSERT_MSG(!(cond), _T(msg))
+    #include <wx/debug.h>
+    #define gxAssert(cond, msg) wxASSERT_MSG(!(cond), _T(msg))
 #else
-  #define gxAssert(cond, msg)
+    #define gxAssert(cond, msg)
 #endif
   
 #endif // gxAssert_wx_h
--- src/adapters/wx/gxLog.h
+++ src/adapters/wx/gxLog.h
@@ -1,9 +1,8 @@
 #ifndef gxLog_wx_h
 #define gxLog_wx_h
 
-#include <wx/log.h>
-
 #if defined(__cplusplus) && defined(__WXDEBUG__)
+    #include <wx/log.h>
     #include "core/geometry/gxGeometry.h"
     static void gxLogRect(const wchar_t* msg, gxRect rect)
     {
