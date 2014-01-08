2fc029cc15dcc07e89feb6af4a2d3d4c0009ab0c
Izhaki <roey@gefri.org>
2014-01-08 02:26:14 +0000

    Some typo corrections

--- src/view/elements/structural/gxChangeNotifier.h
+++ src/view/elements/structural/gxChangeNotifier.h
@@ -5,10 +5,10 @@
 #include "core/observer/gxSubject.h"
 
 /**
- * @brief A view element thtat fires notification whenever its ancestor has
+ * @brief A view element that fires notification whenever its ancestor has
  * changed.
  *
- * Used before anchors were structural elements, but no longer needed really.
+ * TODO: Used before anchors were structural elements, but no longer needed really.
  */
 class gxChangeNotifier: public gxStructural,
                         public virtual gxSubject
